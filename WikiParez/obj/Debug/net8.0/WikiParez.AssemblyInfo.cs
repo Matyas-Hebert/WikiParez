39fbbbc641044aa8d0a28deca660804404a9cc02
Matyas Heber <hebertmatyas@gmail.com>
2025-06-23 23:14:46 +0200

    first commit on parezwiki

--- WikiParez/obj/Debug/net8.0/WikiParez.AssemblyInfo.cs
+++ WikiParez/obj/Debug/net8.0/WikiParez.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25795b63e7fbecd3a7f1ef71fe754597b5c862f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2897f9739360c02bd738a77f4e69d3f39f6523d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
