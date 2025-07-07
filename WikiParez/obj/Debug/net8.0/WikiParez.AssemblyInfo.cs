705abe708817c9ef59cb129aff1d9be9a42a0c49
Matyas Heber <hebertmatyas@gmail.com>
2025-07-07 18:11:09 +0200

    ted uz jenom summary

--- WikiParez/obj/Debug/net8.0/WikiParez.AssemblyInfo.cs
+++ WikiParez/obj/Debug/net8.0/WikiParez.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23372d35b445a7b3bb910a48602fa159ab4a5ef6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1f9940c0d82484d9d5f028e50d974d6f83022cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
