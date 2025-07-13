d8a46be2723150aa062a341bc82ec26a20774fc9
Matyas Heber <hebertmatyas@gmail.com>
2025-07-13 22:41:26 +0200

    small typo fixes

--- WikiParez/obj/Debug/net8.0/WikiParez.AssemblyInfo.cs
+++ WikiParez/obj/Debug/net8.0/WikiParez.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffbbdbfe0a5b139ba156dfe0a86efa836d006698")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063bcde613ddac744c37f32b8f9aabc990d58e87")]
 [assembly: System.Reflection.AssemblyProductAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
