bd5211287978d59afb8c08a2064d8a73575c8dee
Matyas Heber <hebertmatyas@gmail.com>
2025-07-20 21:42:31 +0200

    nejvetsi tma, maja a podlir

--- WikiParez/obj/Debug/net8.0/WikiParez.AssemblyInfo.cs
+++ WikiParez/obj/Debug/net8.0/WikiParez.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+233a91a214cec31a033dc1a182a50ebcfbf279fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11cb875fd67ef0afb60144e63cf45722a7c1c845")]
 [assembly: System.Reflection.AssemblyProductAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
