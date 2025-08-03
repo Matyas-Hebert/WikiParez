653b2371168d0264e07b19304e7f7d377b2f4658
Matyas Heber <hebertmatyas@gmail.com>
2025-08-03 23:02:48 +0200

    vit, over, gid, hralyd

--- WikiParez/obj/Debug/net8.0/WikiParez.AssemblyInfo.cs
+++ WikiParez/obj/Debug/net8.0/WikiParez.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df350b888fc1339749c62606725760d07e2d60c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f083dfcf5fc333c49428f1d1a7f33cd6ba2f01e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
