76cd57fc0e800b176a02ddd4ad60419ead78e53a
Matyas Heber <hebertmatyas@gmail.com>
2025-09-06 22:24:24 +0200

    nove mistnosti (:

--- WikiParez/obj/Debug/net8.0/WikiParez.AssemblyInfo.cs
+++ WikiParez/obj/Debug/net8.0/WikiParez.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f90c9e763958785e8e6390d4802114f5717fcdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771a2f745ccd588bf962b3d4985c39f1968a7df8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WikiParez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
